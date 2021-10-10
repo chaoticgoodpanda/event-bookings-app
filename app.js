0011327199f8b7ce7cc651ba8a3bf09505151f8f
chaoticgoodpanda <michael.lwin@koekoetech.com>
2021-10-11 00:19:56 +0200

    Crafted first MongoDB writes and queries.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const Event = require('./models/event');
+
 // will generate graphQL schema object based on parameters we pass in below
 const { buildSchema } = require('graphql');
 // middleware that takes incoming requests, funnels through graphQL resolver
@@ -9,8 +11,6 @@ const {graphqlHTTP} = require("express-graphql");
 
 // creates an express app Object to start node.js service on 
 const app = express();
-
-const events = [];
 
 // bodyParser middleware to parse json bodies
 app.use(bodyParser.json());
@@ -51,19 +51,33 @@ app.use('/graphql',
     // points to JS object that has all the resolver functions in it
     rootValue: {
         events: () => {
-            return events;
+            // empty find() returns all entries in collection
+            return Event.find().then(events => {
+                return events.map(event => {
+                    // need to convert _id to proper string id to make it readable by graphQL
+                    // mongoose has special .id property to abstract _id to id
+                    return {...event._doc, _id: event.id};
+                });
+            }).catch(err => {
+                throw err;
+            })
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-            };
-            console.log(event);
-            events.push(event);
-            return event;
+                date: new Date(args.eventInput.date)
+            });
+            // provided by mongoose pkg -- hit DB and write our data into the DB
+            return event.save().then(result => {
+                console.log(result);
+                // returns all core properties that make up our document object
+                return {...result._doc};
+            }).catch(err => {
+                console.log(err);
+                throw err;
+            });
         }
     },
         // we visit special URL when we use the graphQL calls
@@ -72,7 +86,8 @@ app.use('/graphql',
 }));
 
 // establish connect to MongoDB server
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@dwmccluster.8vt7g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}
+@dwmccluster.8vt7g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
     .then(() => {
         app.listen(3000);
     }).catch(err => {
