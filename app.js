1630f3d3f38459f3e90d7524578ee24944892dee
chaoticgoodpanda <michael.lwin@koekoetech.com>
2021-10-10 22:34:51 +0200

    Added GraphQL codegen but haven't set up yet

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const {graphqlHTTP} = require("express-graphql");
 // creates an express app Object to start node.js service on 
 const app = express();
 
+const events = [];
+
 // bodyParser middleware to parse json bodies
 app.use(bodyParser.json());
 
@@ -17,12 +19,27 @@ app.use('/graphql',
     graphqlHTTP({
     // needs to point at a valid graphQL schema
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+    
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
         
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
         
         schema {
@@ -33,11 +50,19 @@ app.use('/graphql',
     // points to JS object that has all the resolver functions in it
     rootValue: {
         events: () => {
-            return ['Doctor Telemedicine', 'Teacher Online Lesson', 'Nurse Telemedicine', 'Midwife Telemedicine'];
+            return events;
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            };
+            console.log(event);
+            events.push(event);
+            return event;
         }
     },
         // we visit special URL when we use the graphQL calls
